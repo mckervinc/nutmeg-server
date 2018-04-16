292d3cde16f03788ccc16febbc4128a4ceebd948
Andrew Sun <ajsun12@gmail.com>
2018-04-15 23:16:44 -0400

    adding a refresh route

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -2,6 +2,7 @@ import Controller from './Controller';
 import * as passport from 'passport';
 import * as jwt from 'jsonwebtoken';
 import * as createError from 'http-errors'
+import * as _ from 'lodash'
 
 const controller = new Controller()
 
@@ -9,9 +10,13 @@ controller.addRoute({
     method: 'post',
     route: '/',
     callback: (params, req, res) => {
-        const user = {
-            ...req.user
-        }
+        const user = _.pick(req.user, [
+            'id',
+            'first_name',
+            'last_name',
+            'email',
+            'username'
+        ])
         return jwt.sign(user, process.env.JWT_SECRET)
     }
 })
--- src/controllers/RefreshController.ts
+++ src/controllers/RefreshController.ts
@@ -1,4 +1,6 @@
 import Controller from './Controller'
+import * as jwt from 'jsonwebtoken'
+import * as _ from 'lodash'
 
 const controller = new Controller()
 
@@ -6,7 +8,16 @@ controller.addRoute({
     method: 'get',
     route: '/',
     callback: (params, req, res) => {
-        if (req.user) return 'Success'
+        if (req.user) {
+            const user = _.pick(req.user, [
+                'id',
+                'first_name',
+                'last_name',
+                'email',
+                'username'
+            ])
+            return jwt.sign(user, process.env.JWT_SECRET)
+        }
     }
 })
 
