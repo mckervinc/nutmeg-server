f16e5887d87e8caff32db98492c6ae3ecf8d9d0f
Andrew Sun <ajsun12@gmail.com>
2018-05-08 18:21:28 -0400

    wip

--- src/drivers/socket.ts
+++ src/drivers/socket.ts
@@ -2,9 +2,11 @@ const socketHandler = (io: SocketIO.Server) => {
 
     io.on('connection', socket => {
         console.log('user connected', socket.id)
+        const challenge = socket.handshake.query.challenge
+        socket.join(challenge)
         socket.on('message', msg => {
-            console.log(msg)
-            io.emit('message', msg)
+            console.log(challenge)
+            io.to(challenge).emit('message', msg)
         })
 
         socket.on('disconnect', () => {
